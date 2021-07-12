5a016db604c026dc1c428434fc018153c3c93d9b
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-12 11:30:27 +0300

    Pycache

--- extras/envs.sh
+++ extras/envs.sh
@@ -5,6 +5,7 @@ export QT_QPA_PLATFORMTHEME="qt5ct"
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
+export PYTHONPYCACHEPREFIX=$HOME/.cache/cpython/
 export PYTHONSTARTUP=$HOME/.pythonrc
 export PYTHON_PATH=$HOME/dev/python/PYTHON_PATH
 
