1c7479ca5c7140d00a4d134b8d0ca962aab7d2ac
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-12 11:29:03 +0300

    Pyclean

--- extras/functions.sh
+++ extras/functions.sh
@@ -1,8 +1,13 @@
-_fzf_compgen_path() {
-    fd . "$1"
+pyclean () {
+		find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 }
+
+_fzf_compgen_path() {
+		fd . "$1"
+}
+
 _fzf_compgen_dir() {
-    fd --type d . "$1"
+		fd --type d . "$1"
 }
 
 
