ecfa32543eab6c9b8ef947bcb15c8af7b001debe
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-12 19:56:05 +0300

    i3 status

--- setupScripts/configSetup.sh
+++ setupScripts/configSetup.sh
@@ -6,5 +6,6 @@ ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/fish ~/.config/fish
 ln -s ~/dotfiles/config/clangd ~/.config/clangd
 ln -s ~/dotfiles/config/i3 ~/.config/i3
+ln -s ~/dotfiles/config/i3status ~/.config/i3status
 ln -s ~/dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
