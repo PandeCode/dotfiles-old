da2466ee5bc212eaf0fa3a7ae86775d91d8ee783
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-12 10:50:35 +0300

    Better tmux support

--- tmuxSetup.sh
+++ tmuxSetup.sh
@@ -2,13 +2,11 @@
 currDir=$(pwd)
 
 cd ~
-git clone https://github.com/samoshkin/tmux-config
+rm -fr ~/tmux-config ~/.tmux ~/.tmux.conf
+
+git clone https://github.com/PandeCode/tmux-config
 cd ~/tmux-config
 ./install.sh
 cd ~
-
-cd ~/dotfiles
-rm ~/.tmux/tmux.conf
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux/tmux.conf
 
 cd $currDir
