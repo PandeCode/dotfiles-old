fb6ca17ca68126fb068e0da7bae95667e7a098a9
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-13 14:46:36 +0300

    Kitty Terminal

--- setupScripts/configSetup.sh
+++ setupScripts/configSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0
+rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0 ~/.config/kitty
 
 ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/fish ~/.config/fish
@@ -9,3 +9,4 @@ ln -s ~/dotfiles/config/i3 ~/.config/i3
 ln -s ~/dotfiles/config/i3status ~/.config/i3status
 ln -s ~/dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
+ln -s ~/dotfiles/config/kitty ~/.config/kitty
