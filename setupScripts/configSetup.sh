90947e0f46a4e5b41ad5f449d1dbf26c45889e02
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-13 22:59:45 +0300

    Rofi theme | font

--- setupScripts/configSetup.sh
+++ setupScripts/configSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0 ~/.config/kitty ~/.config/polybar ~/.config/i3status
+rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0 ~/.config/kitty ~/.config/polybar ~/.config/i3status ~/.config/rofi
 
 ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/fish ~/.config/fish
@@ -11,3 +11,4 @@ ln -s ~/dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
 ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
 ln -s ~/dotfiles/config/kitty ~/.config/kitty
 ln -s ~/dotfiles/config/polybar ~/.config/polybar
+ln -s ~/dotfiles/config/rofi ~/.config/rofi
