d1d935ca8a3c9d9957e198de81df2a9976f5e875
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-13 14:12:00 +0300

    Theme and fish

--- getTheme.sh
+++ getTheme.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/dracula/qt5/master/Dracula.conf -o ~/.config/qt5ct/colors/Dracula.conf
+cwd=$(pwd)
+
+# Qt5
+mkdir -p ~/.config/qt5ct/colors/
+cd ~/.config/qt5ct/colors/
+rm -f Dracula.conf
+wget https://raw.githubusercontent.com/dracula/qt5/master/Dracula.conf
+
+# GTK
+mkdir -p ~/.themes/
+cd ~/.themes/
+wget https://github.com/dracula/gtk/archive/master.zip
+7z x master.zip
+rm -f master.zip
+
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
+# Icons
+mkdir -p ~/.icons/
+cd ~/.icons/
+wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+7z x Dracula.zip
+rm -f Dracula.zip
+
+gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+
+cd $cwd
