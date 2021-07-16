5727d041ab22ee4061698e38445af8b379479b00
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-16 09:46:44 +0300

    fish

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,3 @@
-# Defined interactively
 function fish_prompt
     # This prompt shows:
     # - green lines if the last return command is OK, red otherwise
@@ -17,8 +16,8 @@ function fish_prompt
 
     # To:
     # ┬─[nim@Hattori:~/w/dashboard]─[11:37:14]─[V:django20]─[G:master↑1|●1✚1…1]─[B:85%, 05:41:42 remaining]
-    # │ 2    15054    0%    arrêtée    sleep 100000
-    # │ 1    15048    0%    arrêtée    sleep 100000
+    # │ 2	15054	0%	arrêtée	sleep 100000
+    # │ 1	15048	0%	arrêtée	sleep 100000
     # ╰─>$ echo there
 
     set -l retc red
@@ -29,8 +28,8 @@ function fish_prompt
 
     function _nim_prompt_wrapper
         set retc $argv[1]
-        set -l field_name $argv[2]
-        set -l field_value $argv[3]
+        set field_name $argv[2]
+        set field_value $argv[3]
 
         set_color normal
         set_color $retc
@@ -50,23 +49,19 @@ function fish_prompt
     echo -n '┬─'
     set_color -o green
     echo -n [
-
-    if functions -q fish_is_root_user; and fish_is_root_user
+    if test "$USER" = root -o "$USER" = toor
         set_color -o red
     else
         set_color -o yellow
     end
-
     echo -n $USER
     set_color -o white
     echo -n @
-
     if [ -z "$SSH_CLIENT" ]
         set_color -o blue
     else
         set_color -o cyan
     end
-
     echo -n (prompt_hostname)
     set_color -o white
     echo -n :(prompt_pwd)
@@ -76,32 +71,6 @@ function fish_prompt
     # Date
     _nim_prompt_wrapper $retc '' (date +%X)
 
-    # Vi-mode
-    # The default mode prompt would be prefixed, which ruins our alignment.
-    function fish_mode_prompt
-    end
-
-    if test "$fish_key_bindings" = fish_vi_key_bindings
-        or test "$fish_key_bindings" = fish_hybrid_key_bindings
-        set -l mode
-        switch $fish_bind_mode
-            case default
-                set mode (set_color --bold red)N
-            case insert
-                set mode (set_color --bold green)I
-            case replace_one
-                set mode (set_color --bold green)R
-                echo '[R]'
-            case replace
-                set mode (set_color --bold cyan)R
-            case visual
-                set mode (set_color --bold magenta)V
-        end
-        set mode $mode(set_color normal)
-        _nim_prompt_wrapper $retc '' $mode
-    end
-
-
     # Virtual Environment
     set -q VIRTUAL_ENV_DISABLE_PROMPT
     or set -g VIRTUAL_ENV_DISABLE_PROMPT true
@@ -109,7 +78,7 @@ function fish_prompt
     and _nim_prompt_wrapper $retc V (basename "$VIRTUAL_ENV")
 
     # git
-    set -l prompt_git (fish_git_prompt | string trim -c ' ()')
+    set prompt_git (fish_git_prompt | string trim -c ' ()')
     test -n "$prompt_git"
     and _nim_prompt_wrapper $retc G $prompt_git
 
@@ -123,14 +92,12 @@ function fish_prompt
 
     # Background jobs
     set_color normal
-
     for job in (jobs)
         set_color $retc
         echo -n '│ '
         set_color brown
         echo $job
     end
-
     set_color normal
     set_color $retc
     echo -n '╰─>'
