08f7e6b86dd8535b5f5c07a8f31796653a52b622
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-16 09:49:33 +0300

    autojump

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,3 +9,7 @@ set -g fish_emoji_width 1
 set -g fish_ambiguous_width 1
 
 source ~/.config/fish/functions/userFuntions.fish
+
+if test -f /home/shawn/.autojump/share/autojump/autojump.fish
+    . /home/shawn/.autojump/share/autojump/autojump.fish
+end
