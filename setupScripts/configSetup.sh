98a4da589c2146589b93e1f01003e3a22576c8e8
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-17 20:51:51 +0300

    Picom

--- setupScripts/configSetup.sh
+++ setupScripts/configSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0 ~/.config/kitty ~/.config/polybar ~/.config/i3status ~/.config/rofi ~/.config/gtk-3.20
+rm -fr ~/.config/clangd ~/.config/fish ~/.config/fontconfig ~/.config/i3 ~/.config/gtk-2.0 ~/.config/gtk-3.0 ~/.config/kitty ~/.config/polybar ~/.config/i3status ~/.config/rofi ~/.config/gtk-3.20 ~/.config/picom
 
 ln -s ~/dotfiles/config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/config/fish ~/.config/fish
@@ -13,3 +13,4 @@ ln -s ~/dotfiles/config/gtk-2.0 ~/.config/gtk-2.0
 ln -s ~/dotfiles/config/kitty ~/.config/kitty
 ln -s ~/dotfiles/config/polybar ~/.config/polybar
 ln -s ~/dotfiles/config/rofi ~/.config/rofi
+ln -s ~/dotfiles/config/picom ~/.config/picom
