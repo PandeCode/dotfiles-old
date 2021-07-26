a958de91bb066454aac490d5eba3f3a1d5a95c3b
PandeCode <pandeshawnbenjamin@gmail.com>
2021-07-26 10:37:16 +0300

    Autostart xflux | rm picom conf | ipc sock i3

--- config/i3/background.sh
+++ config/i3/background.sh
@@ -1,3 +1,4 @@
 #!/bin/env bash
 
 watch -n 900 feh --randomize --bg-fill ~/Pictures/Wallpapers/* &>/dev/null &
+disown
--- extras/autostart.sh
+++ extras/autostart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo -E chmod 777 /sys/class/backlight/intel_backlight/brightness
 sudo sysctl dev.i915.perf_stream_paranoid=0
-xflux -l 0
