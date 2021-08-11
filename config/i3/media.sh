bf809aa8ed57fcafa75df7d58b18a8d432f82236
PandeCode <pandeshawnbenjamin@gmail.com>
2021-08-11 09:58:34 +0300

    Conky | i3blocks | mod+e i3 explorer | github readme table of contents

--- config/i3/media.sh
+++ config/i3/media.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-playerctl metadata --format '{{status}} : {{playerName}} {{position}} {{volume}} {{album}} {{artist}} {{title}}'
+playerctl metadata --format '{{status}}_{{playerName}}_{{position}}_{{volume}}_{{album}}_{{artist}}_{{title}}'
