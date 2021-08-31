3748c638f3276cca091cbee0749f83fe4cdea0d1
PandeCode <pandeshawnbenjamin@gmail.com>
2021-08-31 17:09:33 +0300

    envs fzf

--- extras/envs.sh
+++ extras/envs.sh
@@ -1,3 +1,4 @@
+export TERMINAL=/usr/bin/kitty
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 export EDITOR=/usr/bin/nvim
@@ -23,9 +24,12 @@ unset FZF_DEFAULT_COMMAND
 unset FZF_DEFAULT_OPTS
 unset FZF_CTRL_T_COMMAND
 
-#--color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+preview="([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200"
 export FZF_DEFAULT_OPTS="
---color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
+--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+--color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+--color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
 --layout=reverse
 --info=inline
 --height=80%
@@ -33,7 +37,7 @@ export FZF_DEFAULT_OPTS="
 --border
 --ansi
 --preview-window=:hidden
---preview '([[ -f {} ]] && (~/.local/bin/bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+--preview '$preview'
 --prompt='∼ ' --pointer='▶' --marker='✓'
 --bind '?:toggle-preview'
 --bind 'ctrl-a:select-all'
