3c79c301e1080b2d45212a0fc7bcbdb38c30b7b6
PandeCode <pandeshawnbenjamin@gmail.com>
2021-09-06 15:27:14 +0300

    chromedriver

--- setupScripts/getDrivers.sh
+++ setupScripts/getDrivers.sh
@@ -1,3 +1,7 @@
 #!/bin/env sh
 
-wget "https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_linux64.zip" -o ~/dotfiles/PERSONAL_PATH/chromedriver
+wget "https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_linux64.zip"
+7z x chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+
+mv chromedriver ~/dotfiles/PERSONAL_PATH/
